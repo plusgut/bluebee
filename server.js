3df8dbbafc1d472699d8e6edda3f56d5f51b5814
cjeske <cjeske@Carlo-Jeskes-MacBook-Pro.local>
2011-05-14 16:17:45 +0200

    Made code-cleaning

--- server.js
+++ server.js
@@ -1,113 +1,135 @@
 var bluebee = bluebee || (function(){
-    /////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////
    
-    var
-    ////=============================================================================================
-    // Requirements
+	var
+	////=============================================================================================
+	// Requirements
 
-        sys             = require( "sys" ),
-        http            = require( "http" ),
-        fs              = require( "fs" ),
-        socket		    = require( "socket.io" ),
-        EventEmitter    = require( "events" ).EventEmitter,
-	
-    ////=============================================================================================
-    // Propertys
+		sys		= require( "sys" ),
+		http		= require( "http" ),
+		fs		= require( "fs" ),
+		socket		= require( "socket.io" ),
+		EventEmitter	= require( "events" ).EventEmitter,
 
-        debug = true,
+	////=============================================================================================
+	// Propertys
 
-        system	= {},		// Some System ressources
-        bb	    = {},		// Central object, which is assigned to all modules
-        core	= {},		// Holds all Core-Modules
-        modules	= {},		// Holds all feature-modules
+		debug	=  true,
+
+		system	= {},		// Some System ressources
+		bb	= {},		// Central object, which is assigned to all modules
+		core	= {},		// Holds all Core-Modules
+		modules	= {},		// Holds all feature-modules
+
+	////=============================================================================================
+	// Methods
+
+		////-----------------------------------------------------------------------------------------
+		// Initializer ( Constructor ) 
+		init = function(){
+			// Output 
+			sys.puts( "BlueBee is starting now" );
+
+			//Some initializations
+			loadCore();
+			//loadModules(); #ToDo should be done _after_ the core-module are loaded
+
+			process.on( "uncaughtException", function (err ) {
+				log( err );
+			});
+		},
+
+		////-----------------------------------------------------------------------------------------
+		//Loads all Core Modules
+ 		loadCore = function(){
+			loader( process.cwd() + "/server/core", core );
+		},
         
-    ////=============================================================================================
-    // Methods
+		 ////-----------------------------------------------------------------------------------------
+		//Loads all the other Modules
+ 		loadModules = function(){
+			loader( process.cwd() + "/server/modules", core );
+		},
+
+		////-----------------------------------------------------------------------------------------
+ 		//The loader itself
+	        loader = function( path, modulesObj ){
+			fs.readdir( path, function( err, files){
+				if( err ){
+					log( err );
+				} else {
+					var i = 0;
+					var v = 0;
+					files.forEach( function( file ){
+						try{
+                         				var mod				= new require( path + "/" + file );
+							mod.module.prototype		= new EventEmitter;
+							modulesObj[ file ]		= new mod.module();
+							modulesObj[ file ].ready	= false;
+							log( modulesObj[ file ] );
+
+							modulesObj[ file ].main();
+							log( modulesObj[ file ] );
+							modulesObj[ file ].on( "ready", function( file ){
+                                				log( "oh, someone is ready!" );
+								v++;
+								if( i === v ){
+									log( "doh, everything is ready!" );
+								}
+							});
+							i++;
+						} catch( e ) {
+							log( file + ": " + e, "error" );
+						}
+					});
+				}
+			});	
+		},
+
+		////-----------------------------------------------------------------------------------------
+		// Output debug Messages
+		log = function( content, type){
+
+			if( type == "error" ){
+				//Safe to file #ToDo
+
+				//Print if debug-mode
+				if( debug ){
+					console.log( "---------------------------------------------------------------" );
+					console.log( "ERROR - " + new Date() );
+					console.log( content );
+					console.trace();
+					console.log( "---------------------------------------------------------------" );
+				}
+			} else if( type == "access" ){
+				//Safe to file #ToDo
+
+				//Print if debug-mode
+				if( debug ){
+					console.log( "ACCESS:" );
+					console.log( content );
+				}
+			} else if( debug ){
+				//Print if debug-mode #ToDo safe to file
+				console.log( content );
+			}
+ 		};
         
-        ////-----------------------------------------------------------------------------------------
-        // Initializer ( Constructor ) 
-        init = function(){
-            // Output 
-            sys.puts( "BlueBee is starting now" );
-            
-            //Some initializations
-            loadCore();
-            loadModules();
-            
-            process.on( "uncaughtException", function (err ) {
-                log( err );
-            });
-        },
-        
-        ////-----------------------------------------------------------------------------------------
-        //Loads all Core Modules
-        loadCore = function(){
-            loader( process.cwd() + "/server/core", core );
-        },
-        
-        ////-----------------------------------------------------------------------------------------
-        //Loads all the other Modules
-        loadModules = function(){
-            loader( process.cwd() + "/server/modules", core );
-        },
+	////---------------------------------------------------------------------------------------------
+	;
 
-        ////-----------------------------------------------------------------------------------------
-        //The loader itself
-        loader = function( path, modulesObj ){
-            fs.readdir( path, function( err, files){
-                if( err ){
-                    log( err );
-                } else {
-                    var i = 0;
-                    var v = 0;
-                    files.forEach( function( file ){
-                        try{
-                            modulesObj[ file ]              = require( path + "/" + file );
-                            modulesObj[ file ].m            = new modulesObj[ file ].module(); 
-                            modulesObj[ file ].m.prototype  = new EventEmitter();
-                            modulesObj[ file ].m.ready      = false;
-                            modulesObj[ file ].m.main();
-                            log( modulesObj[ file ] );
+	/////////////////////////////////////////////////////////////////////////////////////////////////
 
-                            modulesObj[ file ].m.event.on( "ready", function( file ){
-                                log( "oh, someone is ready!" );
-                                v++;
-                                if( i === v ){
-                                    log( "doh, everything is ready!" );
-                                }
-                            });
-                            i++;
-                        } catch( e ) {
-                            log( file + ": " + e );
-                        }
-                    });
-                }
-            });	
-        },
+	////=============================================================================================
+	// Call the Constructor
 
-        ////-----------------------------------------------------------------------------------------
-        // Output debug Messages
-        log = function( content){
-            if( debug ){
-                console.log( content );
-            }
-        }
-        
-    ////---------------------------------------------------------------------------------------------
-    ;
+	init();
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////
+	////=============================================================================================
+	// Public API
 
-    ////=============================================================================================
-    // Call the Constructor
+	return {
+	};
 
-    init();
-    
-    ////=============================================================================================
-    // Public API
-        
-    return {
-    };
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////
 })();
--- server/core/controller.js
+++ server/core/controller.js
@@ -1,10 +1,6 @@
-console.log( "THE FUCKING CONTROLLER WAS FUCKING LOADED!" );
 exports.module = function(){
-	var blarg = "tüdelü";
-
-	var main = function(){
+	this.main = function(){
 		console.log( "its teh fuckin main-function of this fuckin module!" );
-	//	this.emit( "ready", "blarg");
-
+		console.log( this.emit( "ready", "blarg") );
 	}
 }
