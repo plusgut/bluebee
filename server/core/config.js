6bfb0a4b66c28e3397dc0eb94f520b5faef5fe56
cjeske <cjeske@Carlo-Jeskes-MacBook-Pro.local>
2011-06-13 16:51:33 +0200

    Made a little code-cleaning thingy

--- server/core/config.js
+++ server/core/config.js
@@ -5,14 +5,13 @@ exports.module = function(){
 	////-----------------------------------------------------------------------------------------
  	//The Constructor
 	this.main = function( cb ){
-		fs.readFile( process.cwd() + "/config.json" , "binary", function( err, file ) {
+		fs.readFile( bb.path + "/config.json" , "binary", function( err, file ) {
 			if( err ){
 				bb.log( "Configuration-file loading went wrong, shutting down bb [" + err + "]", "error" );
 				bb.log( "Configuration-file loading went wrong, shutting down bb", "prompt" );
 			} else {
 				try{
 					bb.conf		= JSON.parse( file );
-					bb.conf.path	= process.cwd();
 					cb();
 				} catch( err ) {
 					bb.log( "Configuration-file was invalid, shutting down bb  [" + err + "]", "error" );
