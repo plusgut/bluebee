b59ef1279d19fad0e4d1615b3a4463973298ccc1
cjeske <c.jeske@blooparksystems.de>
2011-08-05 13:00:50 +0200

    made mergandice for stylesheets

--- server/modules/mergandice.js
+++ server/modules/mergandice.js
@@ -5,47 +5,65 @@ exports.module = function(){
 	}
 
 	this.on( "stylesheet.css", function( req ){
-		var content = "";
-		bb.modules.mergandice.readDir( "client", "css", null, function( path ){
-			bb.log( path );
-		}, function(){
-			bb.log( "blub" );
+		bb.modules.mergandice.readDir( "client", "css", null, function( results ){
+			var content = "";
+			var i = results.length;
+			results.forEach( function( path ){
+				fs.readFile( bb.path + "/" + path, "utf8", function( err, file ) {
+					if ( err ) {
+						bb.log( err, "error" );
+					} else {
+						content += "/* Content of " +path + "*/\n\n";
+						content += file
+					}
+					if( !--i ){
+						req.write( content );
+					}
+				})
+			});
 		});
-		req.write( "blarg" );
 	});
 
 	this.on( "javascript.js", function( req ){
-		bb.modules.mergandice.readDir( "client", "js", "client/js/libs/sproutcore" ,function( path ){
+		bb.modules.mergandice.readDir( "client", "js", "client/js/libs/sproutcore" ,function( results ){
 			bb.log( path );
-		}, function(){
-			bb.log( "blub" );
 		});
 		req.write( "blarg" );
 	});
 
-	this.readDir = function( path, fileType, exclude, cb, finishCb ){
+	this.readDir = function( path, fileType, exclude, cb ){
+		var results = [];
 		fs.readdir( bb.path + "/" + path, function( err, files ){
 			if( err ){
 				bb.log( err, "error" );
 			} else {
+				var i = files.length;
 				files.forEach( function( file ){
 					fs.stat( bb.path + "/" + path + "/" + file, function( err, stat ){
 						if( err ){
 							bb.log( err, "error" );
 						} else {
+							var newPath = path + "/" + file;
 							if( stat && stat.isDirectory() ){
-								var newPath = path + "/" + file;
 								if( newPath != exclude ){
-									bb.modules.mergandice.readDir( newPath, fileType, exclude, cb, finishCb );
+									bb.modules.mergandice.readDir( newPath, fileType, exclude, function( res ){
+										results = results.concat( res );
+										if( !--i ){
+											cb( results );
+										}
+									});
 								}
 							} else {
 								var splittedFile = file.split( "." );
 								if( splittedFile[ splittedFile.length - 1 ] == fileType ){
-									cb( path + "/" + file );
+									results.push( newPath );
+								}
+								if( !--i ){
+									cb( results );
 								}
 							}
 						}
-					})					
+					})
 				})
 			}
 		})
