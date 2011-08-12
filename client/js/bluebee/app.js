eb9db9d582e45f811ab96361d5a76688a0c0572d
plusgut <github@webentwickler2-0.de>
2011-08-12 23:52:43 +0200

    first trys to make thoth running..

--- client/js/bluebee/app.js
+++ client/js/bluebee/app.js
@@ -1,15 +1,27 @@
 var BB = SC.Application.create();
 
+
+SC.$(document).ready(function() {//I had to use this one, instead of BB.ready.. because frameworks were not loaded
+	BB.DataSource = ThothSC.DataSource.extend({ThothHost:"localhost",ThothPort:"8080"})
+	BB.store = SC.Store.create({ commitRecordsAutomatically: true}).from('BB.DataSource');
+	BB.Data.create( { title: "blub" } );
+});
+
+BB.debug = true;
+
 BB.Index = SC.View.extend({
 	mouseDown: function() {
 		BB.log( "ah, clicki is working" );
 	},
 });
 
-BB.debug = true;
-
 BB.log = function log( content ){
 	if( BB.debug ){
 		console.log( content )
 	}
 };
+
+
+BB.Data = SC.Record.extend({
+	title: SC.Record.attr(String),
+});
