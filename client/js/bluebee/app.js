146f84bb215557ad767141a8347b0e93e4b66113
plusgut <github@webentwickler2-0.de>
2011-08-13 12:08:22 +0200

    deleted thoth-sc, don't know why its not working in sc2.0

--- client/js/bluebee/app.js
+++ client/js/bluebee/app.js
@@ -1,27 +1,31 @@
-var BB = SC.Application.create();
+var bb = SC.Application.create();
 
 
 SC.$(document).ready(function() {//I had to use this one, instead of BB.ready.. because frameworks were not loaded
-	BB.DataSource = ThothSC.DataSource.extend({ThothHost:"localhost",ThothPort:"8080"})
+	BB.DataSource = ThothSC.DataSource.extend( { connectUsing: ThothSC.WEBSOCKET, ThothHost:"localhost",ThothPort:"8080", ThothURLPrefix: '/thoth'} )
+//	BB.DataSource = ThothSC.WebSocketClient.extend( { ThothHost:"localhost",ThothPort:"8080", ThothURLPrefix: '/thoth'} )
 	BB.store = SC.Store.create({ commitRecordsAutomatically: true}).from('BB.DataSource');
-	BB.Data.create( { title: "blub" } );
+	var data = BB.store.createRecord( BB.Data, { title: "blub"} );
+/*	BB.store.find( SC.Query.local( BB.Data ) ).forEach( function( con ){
+		console.log( con.get( "title" ) );
+	});*/
 });
 
-BB.debug = true;
+bb.debug = true;
 
-BB.Index = SC.View.extend({
+bb.Index = SC.View.extend({
 	mouseDown: function() {
 		BB.log( "ah, clicki is working" );
 	},
 });
 
-BB.log = function log( content ){
+bb.log = function log( content ){
 	if( BB.debug ){
 		console.log( content )
 	}
 };
 
 
-BB.Data = SC.Record.extend({
+bb.Data = SC.Record.extend({
 	title: SC.Record.attr(String),
 });
