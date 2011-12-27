386fad67615865dced1116a1ff49e5fb2904d984
plusgut <github@webentwickler2-0.de>
2011-12-27 21:08:34 +0100

    moved the register function to the corresponding controller

--- client/js/bluebee/controllers/index.js
+++ client/js/bluebee/controllers/index.js
@@ -3,5 +3,12 @@ bb.controllers.index = Em.Object.create({
 		user: Em.Object.create(),
 		userName: "foo",
 		userPass: "bar"
-	})
+	}),
+	
+	register: function(){
+		bb.log( this.content.get( "userName") );
+		var userName = this.content.get( "userName");
+		var userPass = this.content.get( "userPass");
+		this.content.set( "user", bb.store.createRecord( bb.User, { name: userName , pass: userPass } ) );
+	}
 });
--- client/js/bluebee/views/index.js
+++ client/js/bluebee/views/index.js
@@ -3,10 +3,7 @@ bb.views.index = Em.View.extend({
 	userPassBinding: "bb.controllers.index.content.userPass",
 	registerView: Em.View.extend({
 		mouseDown: function() {
-			bb.log( bb.controllers.index.content.get( "userName") );
-			var userName = bb.controllers.index.content.get( "userName");
-			var userPass = bb.controllers.index.content.get( "userPass");
-			bb.controllers.index.content.set( "user", bb.store.createRecord( bb.User, { name: userName , pass: userPass } ) );
+			bb.controllers.index.register();
 		}
 	})
 });
