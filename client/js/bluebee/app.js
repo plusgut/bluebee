d51ffdec17310a914c0aa5b0d565952d50788af9
plusgut <github@webentwickler2-0.de>
2011-12-27 19:01:02 +0100

    first try to setup the em-bindings

--- client/js/bluebee/app.js
+++ client/js/bluebee/app.js
@@ -2,20 +2,14 @@ var bb = Em.Application.create({
 	store: Em.Store.create({commitRecordsAutomatically: YES}).from('bb.DataSource')
 });
 
-user = null;
+bb.controllers	= {};
+bb.models	= {};
+bb.views	= {};
 
 Em.$(document).ready(function() {//I had to use this one, instead of BB.ready.. because frameworks were not loaded
-	user = bb.store.createRecord( bb.User, { name: "bar" } );
 });
 
 bb.debug = true;
-
-bb.Index = Em.View.extend({
-	mouseDown: function() {
-		bb.log( "ah, clicki is working" );
-		user.set( "name", "blarg" );
-	},
-});
 
 bb.log = function log( content, type ){
 	if( type == "error" ){
